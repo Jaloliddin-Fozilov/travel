bed1b7a0d567ad5e48837d5eab8025086f2d6774
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-10-05 22:56:51 +0500

    form create

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -10,6 +12,7 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  File? _imageFile;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +23,49 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+                child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Joy nomi',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                    width: double.infinity,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageFile != null
+                        ? Image.file(_imageFile!)
+                        : const Text('Rasm yuklanmagan'),
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.image_search),
+                    label: const Text('Rasm yuklash'),
+                  )
+                ],
+              ),
+            )),
+          ),
           ElevatedButton(
             onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+            ),
             child: const Text('Qo\'shish'),
           )
         ],
